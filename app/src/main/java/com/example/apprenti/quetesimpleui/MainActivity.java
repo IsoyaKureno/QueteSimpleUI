91dc7a8b3ec2990ce8631c6b5ace9a45ab944446
Germain Caule <g.caule31@outlook.fr>
2017-09-07 17:11:49 +0200

    Ajout title bar

--- app/src/main/java/com/example/apprenti/quetesimpleui/MainActivity.java
+++ app/src/main/java/com/example/apprenti/quetesimpleui/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.apprenti.quetesimpleui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -10,22 +13,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CheckBox maCheckBox = (CheckBox) findViewById(R.id.CheckBox);
+        CheckBox maCheckBox = (CheckBox)findViewById(R.id.CheckBox);
         maCheckBox.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                EditText edit4 = (EditText) findViewById(R.id.editText4);
-                EditText edit5 = (EditText) findViewById(R.id.editText5);
-                Button button = (Button) findViewById(R.id.button);
+                EditText edit4 = (EditText)findViewById(R.id.editText4);
+                EditText edit5 = (EditText)findViewById(R.id.editText5);
+                Button button = (Button)findViewById(R.id.button);
 
                 if (((CheckBox) v).isChecked()) {
 
@@ -42,21 +45,21 @@ public class MainActivity extends Activity {
             }
         });
 
-        Button monBouton = (Button) findViewById(R.id.button);
+        Button monBouton = (Button)findViewById(R.id.button);
         monBouton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                EditText edit4 = (EditText) findViewById(R.id.editText4);
-                EditText edit5 = (EditText) findViewById(R.id.editText5);
+                EditText edit4 = (EditText)findViewById(R.id.editText4);
+                EditText edit5 = (EditText)findViewById(R.id.editText5);
 
                 if(edit4.getText().toString().trim().length() == 0 || edit5.getText().toString().trim().length() == 0) {
                     Toast.makeText(MainActivity.this,"Tu dois renseigner le nom et le prénom !",Toast.LENGTH_SHORT).show();
                 }
                 else
                 {
-                    TextView textView = (TextView) findViewById(R.id.textView);
+                    TextView textView = (TextView)findViewById(R.id.textView);
                     textView.setVisibility(View.VISIBLE);
                     textView.setText("Félicitations, " + edit4.getText() + " " + edit5.getText() + "!");
                 }
